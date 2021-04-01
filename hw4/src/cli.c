8e8a8f874152c598705b13a4abd5beb74074ec5d
Gianmaria Biselli <gbiselli@cs.stonybrook.edu>
2021-03-31 20:23:41 -0400

    Finished printers, printer and type command.

--- hw4/src/cli.c
+++ hw4/src/cli.c
@@ -11,6 +11,15 @@
 #include "conversions.h"
 #include "sf_readline.h"
 
+typedef struct printer {
+	int id;
+	char* name;
+	PRINTER_STATUS status;
+	FILE_TYPE* file_type;
+} PRINTER;
+
+PRINTER printers[MAX_PRINTERS];
+
 int num_of_args(char *args) {
 	int argc = 0;
 
@@ -29,6 +38,7 @@ void error_message(int argc, int req, char *command) {
 
 int run_cli(FILE *in, FILE *out)
 {
+	int uid = 0;
     while(1) {
     	char* input = sf_readline("imp>");
     	int argc = num_of_args(input);
@@ -42,47 +52,95 @@ int run_cli(FILE *in, FILE *out)
     		count++;
   		}
 
-  		//printf("%p, %p", out, stdout);
-
     	if(out != stdout) {
     		//TODO
     	}
     	if (strcmp(input, "quit") == 0) {
+    		if (argc != 0) {
+    			error_message(argc, 0, "quit");
+    			sf_cmd_error("argc count");
+    			continue;
+    		}
+    		sf_cmd_ok();
     		return -1;
 
     	}else if(strcmp(input, "help") == 0) {
+    		if (argc != 0) {
+    			error_message(argc, 0, "help");
+    			sf_cmd_error("argc count");
+    			continue;
+    		}
     		printf("Commands are:\nhelp quit type printer conversion printers jobs print cancel disable enable pause resume\n");
-
+    		sf_cmd_ok();
     	}else if(strncmp(input, "type", 4) == 0) {
     		if (argc != 1) {
     			error_message(argc, 1, "type");
+    			sf_cmd_error("argc count");
     			continue;
     		}
-    		if (define_type(arguments[0]) == NULL){
+    		FILE_TYPE *res = define_type(arguments[1]);
+
+    		if (res == NULL){
     			printf("New file could not be defined.");
     		}
 
-    	}else if(strncmp(input, "printer", 7) == 0) {
-    		if (argc != 2) {
-    			error_message(argc, 2, "printer");
+    		sf_cmd_ok();
+
+    	}else if(strncmp(input, "printers", 8) == 0) {
+    		if (argc != 0) {
+    			error_message(argc, 0, "conversions");
+    			sf_cmd_error("argc count");
     			continue;
     		}
-    		FILE_TYPE *file_res = find_type(arguments[2]);
 
+    		for(int i=0; i < MAX_PRINTERS; i++) {
+    			if(printers[i].name == NULL) {
+    				break;
+    			}
+    			printf("PRINTER: id=%d, name=%s, type=%s, status=%s\n", printers[i].id, printers[i].name, printer_status_names[printers[i].status], printers[i].file_type->name);
+    		}
+
+    		sf_cmd_ok();
+    	}else if(strncmp(input, "printer", 7) == 0) {
+
+    		if (argc != 2) {
+    			error_message(argc, 2, "printer");
+    			sf_cmd_error("argc count");
+    			continue;
+    		}
+
+    		char * printer_name = arguments[1];
+    		FILE_TYPE *file_res = find_type(arguments[2]);
     		if(file_res == NULL) {
     			printf("Unknown file type: %s\n", arguments[2]);
     			continue;
     		}
+    		if (uid == MAX_PRINTERS) {
+    			error_message(argc, 2, "printer");
+    			sf_cmd_error("argc count");
+    			continue;
+    		}
 
+    		PRINTER new_printer = {.id = uid, .name = printer_name, .status = PRINTER_DISABLED, .file_type = file_res};
+    		sf_printer_defined(new_printer.name, new_printer.file_type->name);
+    		printers[uid] = new_printer;
+    		uid++;
+    		printf("PRINTER: id=%d, name=%s, type=%s, status=%s\n", new_printer.id, new_printer.name, printer_status_names[new_printer.status], new_printer.file_type->name);
+    		sf_cmd_ok();
+
+    	}else if(strncmp(input, "conversion", 10) == 0) {
+    		if (argc != 3) {
+    			error_message(argc, 3, "conversions");
+    			sf_cmd_error("argc count");
+    			continue;
+    		}
+    		sf_cmd_ok();
 
     	}
-
     }
     return 0;
     //abort();
 }
-
-//int uid = 1;
 
 
 // FILE_TYPE *define_type(char *name) {
